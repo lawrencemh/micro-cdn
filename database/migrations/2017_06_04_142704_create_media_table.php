213706ae03c91da2392471da94cffe07ddabb424
lawrencemh <lawrencemh@outlook.com>
2017-06-10 17:24:38 +0100

    Add media table migration.

---
+++ database/migrations/2017_06_04_142704_create_media_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('name');
+            $table->json('meta_data')->nullable();
+            $table->string('path');
+            $table->timestamps();
+            
+            // User relation
+            $table->foreign('user_id')->references('id')->on('users')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
