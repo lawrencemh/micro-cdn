dcefa47f402a5afac49327d1d14e52b5ab03362c
lawrencemh <lawrencemh@outlook.com>
2017-06-11 13:23:18 +0100

    Enable basic authentication in CDN app.
    Enabled auth middleware, auth service provider & eloquent in app.php config. Updated user table & seeder with new column api_token. created new IndexControlle
    r route and updated the routes to reflect this.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -31,8 +31,11 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            $attributes = $request->all();
+            $apiToken = $attributes['api_token'] ?? null;
+
+            if (!is_null($apiToken)) {
+                return User::where('api_token', $apiToken)->first();
             }
         });
     }
