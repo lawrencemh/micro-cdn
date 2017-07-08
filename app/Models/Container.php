01a799df8da705653a2a5de1caae4f2dd0e79e86
lawrencemh <lawrencemh@outlook.com>
2017-07-08 20:54:21 +0100

    Add Container model.
    * Created Container model.
    * Added user method to Container model to retreive the user instance the container belongs to.

---
+++ app/Models/Container.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Container extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'containers';
+    
+    /**
+     * Get the user that the container belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+}
