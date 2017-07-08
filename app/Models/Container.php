7f61cee9214345882e8a0b934457838855686ce8
lawrencemh <lawrencemh@outlook.com>
2017-07-08 20:55:21 +0100

    Fix user method in Container model.
    Fixed user method in the Container model to retreive an App\Models\User instance instead of the incorrect App\User instance.

--- app/Models/Container.php
+++ app/Models/Container.php
@@ -20,6 +20,6 @@ class Container extends Model
      */
     public function user()
     {
-        return $this->belongsTo('App\User', 'user_id');
+        return $this->belongsTo('App\Models\User', 'user_id');
     }
 }
