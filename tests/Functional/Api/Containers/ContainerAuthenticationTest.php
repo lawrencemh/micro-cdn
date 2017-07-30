1a9bf6f85001acaf4e8ad61fd66a4e5596494768
lawrencemh <lawrencemh@outlook.com>
2017-07-30 12:03:44 +0100

    Add additional tests to ContainerAuthenticationTest.

--- tests/Functional/Api/Containers/ContainerAuthenticationTest.php
+++ tests/Functional/Api/Containers/ContainerAuthenticationTest.php
@@ -9,7 +9,7 @@ class ContainerAuthenticationTest extends ApiTestCase
     /**
      * Test an unauthorised user cannot create containers.
      */
-    public function testAnNonAuthorisedUserCannotCreateAContainer()
+    public function testANonAuthorisedUserCannotCreateAContainer()
     {
         $this->call('POST', '/containers', [
             'name' => 'testContainer',
@@ -17,5 +17,63 @@ class ContainerAuthenticationTest extends ApiTestCase
 
         $this->seeJsonContains($this->apiUnauthorisedRequestJsonResponse);
         $this->assertResponseStatus(401);
+    }
+
+    /**
+     * Test an unauthorised user cannot view containers.
+     */
+    public function testANonAuthorisedUserCannotViewContainers()
+    {
+        $this->call('get', '/containers');
+
+        $this->seeJsonContains($this->apiUnauthorisedRequestJsonResponse);
+        $this->assertResponseStatus(401);
+    }
+
+    /**
+     * Test an unauthorised user cannot delete a container.
+     */
+    public function testANonAuthorisedUserCannotDeleteAContainer()
+    {
+        $user = factory('App\Models\User')->create();
+        $user->containers()->save(factory('App\Models\Container')->make());
+        $container = $user->containers()->first();
+
+        $this->call('delete', "/containers/{$container->id}");
+
+        $this->seeJsonContains($this->apiUnauthorisedRequestJsonResponse);
+        $this->assertResponseStatus(401);
+
+        $this->seeInDatabase('containers', [
+            'user_id'   => $user->id,
+            'name'      => $container->name,
+        ]);
+    }
+
+    /**
+     * Test an unauthorised user cannot update a container.
+     */
+    public function testANonAuthorisedUserCannotUpdateAContainer()
+    {
+        $user = factory('App\Models\User')->create();
+        $user->containers()->save(factory('App\Models\Container')->make());
+        $container = $user->containers()->first();
+
+        $this->call('patch', "/containers/{$container->id}", [
+            'name'      => 'NewTestName',
+        ]);
+
+        $this->seeJsonContains($this->apiUnauthorisedRequestJsonResponse);
+        $this->assertResponseStatus(401);
+
+        $this->seeInDatabase('containers', [
+            'user_id'   => $user->id,
+            'name'      => $container->name,
+        ]);
+
+        $this->NotSeeInDatabase('containers', [
+            'user_id'   => $user->id,
+            'name'      => 'NewTestName',
+        ]);
     }
 }
