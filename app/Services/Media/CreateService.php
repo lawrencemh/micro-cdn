5285128ec111f7f03378d84c4be2d806b8cec44d
lawrencemh <lawrencemh@outlook.com>
2017-09-14 23:05:07 +0100

    Update media CreateService to save images.
    * Added generateRandomAlphaString method that is a wip and generates a alphanumeric string for the given length.
    * Added generateValidFilePath to generate a valid filepath for the class' file.
    * Added generateValidFileName to generate a valid filename for the class' file.
    * Added generateValidExtension to generate a valid extension for the class' file and tidy up the extension.
    * Added canBeCompressed that checks the file's mime to check if the file is a valid image that can be further compressed.
    * Updated the store method to move the uploaded file using the filename and file path and to create a new media object in storage that is associated with the user's container.

--- app/Services/Media/CreateService.php
+++ app/Services/Media/CreateService.php
@@ -2,8 +2,8 @@
 
 namespace App\Services\Media;
 
-use App\Models\Container;
 use App\Models\Media;
+use App\Models\Container;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use App\Repositories\Contracts\MediaRepositoryInterface;
 
@@ -22,6 +22,20 @@ class CreateService
      * @var \Symfony\Component\HttpFoundation\File\UploadedFile
      */
     protected $file;
+
+    /**
+     * The filename that the media item will be stored as.
+     *
+     * @var string
+     */
+    protected $fileName;
+
+    /**
+     * The filepath that the media item will be stored in.
+     *
+     * @var string
+     */
+    protected $filePath;
 
     /**
      * The media model instance.
@@ -51,6 +65,19 @@ class CreateService
         $this->container       = $container;
         $this->file            = $file;
         $this->media           = new Media;
+        $this->filepath        = $this->generateValidFilePath();
+        $this->fileName        = $this->generateValidFileName();
+    }
+
+    /**
+     * Generate a random alpha string for the given length.
+     *
+     * @param int $length
+     * @return string
+     */
+    protected function generateRandomAlphaString($length = 10)
+    {
+        return substr(md5(microtime()), rand(0, 26), $length);
     }
 
     /**
@@ -60,18 +87,52 @@ class CreateService
      */
     protected function generateValidFilePath()
     {
-        // @todo
+        return $this->generateRandomAlphaString(2);
     }
 
     /**
      * Generate a valid filename to store a file for the given file path.
      *
-     * @param string $filePath
      * @return string
      */
-    protected function generateValidFileName($filePath = '')
+    protected function generateValidFileName()
     {
-        // @todo
+        $randomString  = $this->generateRandomAlphaString(10);
+        $fileExtension = $this->generateValidExtension();
+
+        return $randomString . $fileExtension;
+    }
+
+    /**
+     * Return an extension based on the file's mime or original extension.
+     *
+     * @return string
+     */
+    protected function generateValidExtension()
+    {
+        switch ($this->file->getClientMimeType()) {
+            case 'image/jpeg':
+                return '.jpg';
+                break;
+            case 'image/png':
+                return '.png';
+                break;
+            default:
+                return ".{$this->file->getClientOriginalExtension()}";
+                break;
+        }
+    }
+
+    /**
+     * Check whether the uploaded file is an image that can be compressed.
+     *
+     * @return bool
+     */
+    protected function canBeCompressed()
+    {
+        return in_array($this->file->getClientMimeType(), [
+            'image/jpeg',
+            ]);
     }
 
     /**
@@ -81,8 +142,20 @@ class CreateService
      */
     public function store()
     {
-        // @todo wip
-        $this->file->move(storage_path('images/test'));
+        $this->file->move(
+            storage_path("images/{$this->filePath}"), $this->fileName
+        );
+
+        $this->media->name = $this->fileName;
+        $this->media->container()->associate($this->container);
+        $this->media->path      = "/images{$this->filePath}/{$this->fileName}";
+        $this->media->meta_data = [
+            'has_been_processed' => false,
+            'can_be_compressed'  => $this->canBeCompressed(),
+            'file_mime'          => $this->file->getClientMimeType(),
+        ];
+
+        $this->media->save();
 
         return $this;
     }
