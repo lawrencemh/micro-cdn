8f4db1c8d38c3c1c23f1f58208e0628f9f858238
lawrencemh <lawrencemh@outlook.com>
2017-09-17 11:27:25 +0100

    Add helpers file with public_path helper function.

---
+++ app/helpers.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!function_exists('public_path')) {
+    /**
+     * Get the path to the public dir of the install.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function public_path($path = '')
+    {
+        return removeDoubleForwardSlash(
+            app()->basePath() . '/public/' . ($path ? '/' . $path : $path)
+        );
+    }
+}
+
+if (!function_exists('removeDoubleForwardSlash')) {
+    /**
+     * Replace double forward slashes from the given string.
+     *
+     * @param  string $string
+     * @return string
+     */
+    function removeDoubleForwardSlash($string = '')
+    {
+        return preg_replace('#/+#','/',$string);
+    }
+}
