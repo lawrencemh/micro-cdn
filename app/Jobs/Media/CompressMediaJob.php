6b8aeb46fb56cd93ef030093271f0e881ffbbc2d
lawrencemh <lawrencemh@outlook.com>
2017-12-24 18:25:48 +0000

    Create CompressMediaJob to async call imageOptimiserService.
    * Created job to call the compress method for the given media item using the image optimiser service.

---
+++ app/Jobs/Media/CompressMediaJob.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs\Media;
+
+use App\Jobs\Job;
+use App\Models\Media;
+use App\Services\ImageOptimiserService;
+
+class CompressMediaJob extends Job
+{
+    /**
+     * The media instance.
+     *
+     * @var \App\Models\Media
+     */
+    protected $media;
+
+    /**
+     * The image optimisation service instance.
+     *
+     * @var \App\Services\ImageOptimiserService
+     */
+    protected $imageOptimiserService;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param \App\Models\Media $media
+     * @return void
+     */
+    public function __construct(Media $media)
+    {
+        $this->media = $media;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        (new ImageOptimiserService($this->media))->compress();
+    }
+}
