7d63a3aa1d2bfcea4d77e4a1c89ab3fefec2d9e1
lawrencemh <lawrencemh@outlook.com>
2017-12-26 20:26:03 +0000

    Update IndexController to return index view.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.basic')
+
+@section('title', 'Welcome')
+
+@section('content')
+    <div class="flex-container">
+        <div class="col">
+            <div>
+                <h1>Welcome</h1>
+            </div>
+            <div>
+                <h2>All systems appear to be up and running :)</h2>
+            </div>
+        </div>
+    </div>
+@endsection
