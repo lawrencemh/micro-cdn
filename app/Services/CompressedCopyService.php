aac702f0a68eaa757114b58c29ccaea7dc5e9f78
lawrencemh <lawrencemh@outlook.com>
2017-12-26 18:56:29 +0000

    Create destory method for CompressedCopyService.
    * Created destroy method to override the base service's method.
    * Check and unlink compressed copy image from disk when deleting the entity.

--- app/Services/CompressedCopyService.php
+++ app/Services/CompressedCopyService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Repositories\Contracts\CompressedCopyRepositoryInterface;
+use App\Exceptions\Services\Media\FailedToRemoveFromStorageException;
 
 class CompressedCopyService extends AbstractBaseService
 {
@@ -22,5 +23,27 @@ class CompressedCopyService extends AbstractBaseService
     function __construct(CompressedCopyRepositoryInterface $compressedCopyRepository = null)
     {
         $this->repository = $compressedCopyRepository ?? app(CompressedCopyRepositoryInterface::class);
+    }
+
+    /**
+     * Delete the given entity id using the repository.
+     *
+     * @param int $id
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function destroy(int $id)
+    {
+        $copy = $this->findById($id);
+
+        app('db')->transaction(function () use ($copy) {
+            $this->repository->destroy($copy->id);
+
+            // Physically remove file from storage
+            if (file_exists($copy->getFullLocalPath()) && unlink($copy->getFullLocalPath()) === false) {
+                throw new FailedToRemoveFromStorageException('Failed to remove the file from storage');
+            }
+        });
+
+        return $copy;
     }
 }
