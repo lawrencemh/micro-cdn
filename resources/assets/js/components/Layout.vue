234cc8426aba8d282e9b9d04858e9c342dc7e8c6
lawrencemh <lawrencemh@outlook.com>
2018-01-20 18:21:04 +0000

    Update layout component with new logo.
    * Added logo.svg and updated navbar layout in Layout.vue.
    * Added logOut method.
    * Added mounted methot to set loggedIn status dependent on whether api-token cookie is set.

--- resources/assets/js/components/Layout.vue
+++ resources/assets/js/components/Layout.vue
@@ -3,16 +3,35 @@
         <!-- Navigation -->
         <nav class="navbar navbar-default">
             <div class="container">
-                <div class="col-md-2">LOGO</div>
-                <div class="col-md-10">
 
+                <div class="col-md-4 menu-block">
                     <!-- menu -->
                     <ul class="nav navbar-nav navbar-left">
-                        <li><router-link :to="{ name: 'containers.index' }">Containers</router-link></li>
+                        <li>
+                            <router-link :to="{ name: 'containers.index' }">Containers</router-link>
+                        </li>
                     </ul>
-
                 </div>
+
+                <div class="col-md-4">
+                    <div class="logo-group text-center">
+                        <img src="/logo.svg" alt="">
+                    </div>
+                </div>
+
+                <div class="col-md-4 menu-block menu-block-right">
+                    <!-- Auth menu -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a v-if="loggedIn" @click.prevent='logOut()' href="#">Log out</a>
+                            <router-link v-if="loggedIn === false" :to="{ name: 'account.auth.login' }">Log in
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
+
         </nav>
 
         <div class="container">
@@ -26,12 +45,60 @@
 
 <script>
     export default {
-        //
+        data () {
+            return {
+                loggedIn: false,
+            }
+        },
+
+        mounted() {
+            this.loggedIn = !!this.$cookie.get('api-token');
+        },
+
+        methods: {
+            logOut() {
+                // Remove the api key
+                console.log(this.$cookie.get('api-token'));
+                if (this.$cookie.get('api-token')) {
+                    this.$cookie.delete('api-token');
+                }
+
+                this.loggedIn = false;
+                this.$router.push({name: 'account.auth.login'});
+            }
+        }
+
     }
 </script>
 
 <style>
     .navbar {
         margin-bottom: 15px;
+    }
+
+    .navbar .container {
+        display: flex;
+    }
+
+    .navbar .logo-group img {
+        max-width: 225px;
+        padding: 10px;
+    }
+
+    .navbar svg.logo .brand {
+        fill: #2c3e50;
+    }
+
+    .navbar svg.logo .dots {
+        fill: #2c3e50;
+    }
+
+    .menu-block {
+        display: flex;
+        align-items: center;
+    }
+
+    .menu-block-right {
+        justify-content: flex-end
     }
 </style>
